5bb7c09bf1d42aaf2c26d177394d32c19605cf84
Shevchik <Shev4ik.den@gmail.com>
2014-06-10 02:04:43 +0400

    Cleanup

--- src/tgfix/AnvilInteractFix.java
+++ src/tgfix/AnvilInteractFix.java
@@ -3,16 +3,16 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  */
 
 package tgfix;
@@ -36,24 +36,24 @@ public class AnvilInteractFix implements Listener {
 	public AnvilInteractFix(Config config) {
 		this.config = config;
 	}
-	
+
 	@EventHandler(priority=EventPriority.HIGH,ignoreCancelled=true)
 	public void onInteract(PlayerInteractEvent event) {
 		if (!config.anvilinteractfixenabled) {
 			return;
 		}
-		
+
 		if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
 			return;
 		}
-		
+
 		if (event.getPlayer().getItemInHand().getType() != Material.ANVIL) {
 			return;
 		}
-		
+
 		Player player = event.getPlayer();
 		Block block = event.getClickedBlock();
-		ApplicableRegionSet ars = WGBukkit.getRegionManager(block.getWorld()).getApplicableRegions(block.getLocation()); 
+		ApplicableRegionSet ars = WGBukkit.getRegionManager(block.getWorld()).getApplicableRegions(block.getLocation());
 		if (!ars.canBuild(WGBukkit.getPlugin().wrapPlayer(player))) {
 			event.setCancelled(true);
 			player.sendMessage(ChatColor.RED+"You don't have enough permissions to do this");
