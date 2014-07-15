240e9dd800f4a13a60f6ff57b049a8ed6188276d
Shevchik <Shev4ik.den@gmail.com>
2014-07-16 01:19:24 +0400

    Update to the latest WE api

--- src/tgfix/WorldEditWand.java
+++ src/tgfix/WorldEditWand.java
@@ -4,10 +4,6 @@ import java.util.Iterator;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,13 +12,11 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import com.sk89q.worldedit.LocalPlayer;
 import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.entity.Player;
 
 public class WorldEditWand implements Listener {
 
@@ -36,14 +30,14 @@ public class WorldEditWand implements Listener {
 	}
 
 	private String wandcommand = "//wand";
-	private Material wandmaterial = Material.WOOD_AXE;
+	private org.bukkit.Material wandmaterial = org.bukkit.Material.WOOD_AXE;
 	private String wandname = ChatColor.LIGHT_PURPLE + "Selection wand";
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onCommand(PlayerCommandPreprocessEvent event) {
 		if (event.getMessage().trim().equalsIgnoreCase(wandcommand)) {
-			ItemStack wand = new ItemStack(wandmaterial);
-			ItemMeta im = Bukkit.getItemFactory().getItemMeta(wandmaterial);
+			org.bukkit.inventory.ItemStack wand = new org.bukkit.inventory.ItemStack(wandmaterial);
+			org.bukkit.inventory.meta.ItemMeta im = Bukkit.getItemFactory().getItemMeta(wandmaterial);
 			im.setDisplayName(wandname);
 			wand.setItemMeta(im);
 			event.getPlayer().getInventory().addItem(wand);
@@ -58,20 +52,20 @@ public class WorldEditWand implements Listener {
 			return;
 		}
 
-		Player player = event.getPlayer();
-		ItemStack item = player.getItemInHand();
+		org.bukkit.entity.Player player = event.getPlayer();
+		org.bukkit.inventory.ItemStack item = player.getItemInHand();
 		if (item.getType() == wandmaterial && item.hasItemMeta() && wandname.equalsIgnoreCase(item.getItemMeta().getDisplayName())) {
 			event.setCancelled(true);
-			Block block = event.getBlock();
+			org.bukkit.block.Block block = event.getBlock();
 			if (block != null) {
-				LocalPlayer lplayer = we.wrapPlayer(player);
+				Player bplayer = we.wrapPlayer(player);
 				LocalSession session = we.getSession(player);
 				Vector vector = new Vector(block.getX(), block.getY(), block.getZ());
-		        if (!session.getRegionSelector(lplayer.getWorld()).selectPrimary(vector)) {
-		            lplayer.printError("Position already set.");
+		        if (!session.getRegionSelector(bplayer.getWorld()).selectPrimary(vector)) {
+		            bplayer.printError("Position already set.");
 		            return;
 		        }
-				session.getRegionSelector(lplayer.getWorld()).explainPrimarySelection(lplayer, session, vector);
+				session.getRegionSelector(bplayer.getWorld()).explainPrimarySelection(bplayer, session, vector);
 			}
 		}
 	}
@@ -82,20 +76,20 @@ public class WorldEditWand implements Listener {
 			return;
 		}
 
-		Player player = event.getPlayer();
-		ItemStack item = player.getItemInHand();
+		org.bukkit.entity.Player player = event.getPlayer();
+		org.bukkit.inventory.ItemStack item = player.getItemInHand();
 		if (item.getType() == wandmaterial && item.hasItemMeta() && wandname.equalsIgnoreCase(item.getItemMeta().getDisplayName())) {
 			event.setCancelled(true);
-			Block block = event.getClickedBlock();
+			org.bukkit.block.Block block = event.getClickedBlock();
 			if (block != null) {
-				LocalPlayer lplayer = we.wrapPlayer(player);
+				Player bplayer = we.wrapPlayer(player);
 				LocalSession session = we.getSession(player);
 				Vector vector = new Vector(block.getX(), block.getY(), block.getZ());
-		        if (!session.getRegionSelector(lplayer.getWorld()).selectSecondary(vector)) {
-		            lplayer.printError("Position already set.");
+		        if (!session.getRegionSelector(bplayer.getWorld()).selectSecondary(vector)) {
+		            bplayer.printError("Position already set.");
 		            return;
 		        }
-				session.getRegionSelector(lplayer.getWorld()).explainSecondarySelection(lplayer, session, vector);
+				session.getRegionSelector(bplayer.getWorld()).explainSecondarySelection(bplayer, session, vector);
 			}
 		}
 	}
@@ -106,9 +100,9 @@ public class WorldEditWand implements Listener {
 			return;
 		}
 
-		Entity edamager = event.getDamager();
+		org.bukkit.entity.Entity edamager = event.getDamager();
 		if (edamager instanceof Player) {
-			ItemStack item = ((Player) edamager).getItemInHand();
+			org.bukkit.inventory.ItemStack item = ((org.bukkit.entity.Player) edamager).getItemInHand();
 			if (item.getType() == wandmaterial && item.hasItemMeta() && wandname.equalsIgnoreCase(item.getItemMeta().getDisplayName())) {
 				event.setCancelled(true);
 			}
@@ -121,9 +115,9 @@ public class WorldEditWand implements Listener {
 			return;
 		}
 
-		Iterator<ItemStack> dropsit = event.getDrops().iterator();
+		Iterator<org.bukkit.inventory.ItemStack> dropsit = event.getDrops().iterator();
 		while (dropsit.hasNext()) {
-			ItemStack item = dropsit.next();
+			org.bukkit.inventory.ItemStack item = dropsit.next();
 			if (item.getType() == wandmaterial && item.hasItemMeta() && wandname.equalsIgnoreCase(item.getItemMeta().getDisplayName())) {
 				dropsit.remove();
 			}
